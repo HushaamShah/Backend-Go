2a12e2e3e595ff7763593c013d1e04a6d80cff6e
HushaamShah <hasham393@gmail.com>
2020-07-15 01:26:27 +0500

    CSV File

---
+++ hello.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	//"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	// Open the file
+	csvfile, err := os.Open("data.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile)
+	//r := csv.NewReader(bufio.NewReader(csvfile))
+
+	// Iterate through the records
+
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if record[3] == "08/05/2020"{
+			fmt.Println(record)
+		}
+		
+	}
+}
