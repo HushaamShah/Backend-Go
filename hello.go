9fca1130b9632b71f0057e9ed622679f9e29f70c
HushaamShah <hasham393@gmail.com>
2020-07-15 03:30:06 +0500

    Structures

--- hello.go
+++ hello.go
@@ -7,10 +7,22 @@ import (
 	"io"
 	"log"
 	"os"
+	"encoding/json"
 )
+
+type covid_data struct{
+	testperformed string
+	testpositive string
+	date string
+	discharged string
+	expired string
+	region string
+	admitted string
+}
 
 func main() {
 	// Open the file
+	var data covid_data
 	csvfile, err := os.Open("data.csv")
 	if err != nil {
 		log.Fatalln("Couldn't open the csv file", err)
@@ -31,9 +43,28 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		if record[3] == "08/05/2020"{
-			fmt.Println(record)
-		}
+		if record[2] == "11/03/2020"{
+			slcB, _ := json.Marshal(record)
+			fmt.Println(string(slcB))
 		
+			
+			//fmt.Println(record[0])
+			//fmt.Println("asdfsdf")
+			data.testpositive = record[0]
+			data.testperformed = record[1]
+			data.date = record[2]
+			data.discharged = record[3]
+			data.expired = record[4]
+			data.region = record[5]
+			data.admitted = record[6]
+		}
+		//fmt.Println(data.test_positive)
+		// fmt.Println(data.test_performed)
+		// fmt.Println(data.date)
+		// fmt.Println(data.discharged)
+		// fmt.Println(data.expired)
+		// fmt.Println(data.region)
+		// fmt.Println(data.admitted)
+		// fmt.Println("asdfgh")
 	}
 }
